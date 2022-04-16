f41534dabd05dc737c480b1f4783059017115afd
Gokalp Giray <gokalpgiray24@gmail.com>
2022-04-17 02:18:16 +0300

    gg-7 Feed Scroll Düzenlemesi Yapıldı. Proje Tamamlandı.

--- client/src/components/ChatList.js
+++ client/src/components/ChatList.js
@@ -1,19 +1,20 @@
 import { useChat } from "../context/ChatContext";
 import ChatItem from "./ChatItem";
 import styles from "./styles.module.css";
+import ScrollableFeed from "react-scrollable-feed"
 
 function ChatList() {
   const { messages } = useChat();
 
   return (
     <div className={styles.chatlist}>
-      <div>
+      <ScrollableFeed forceScroll={true}>
         {
           messages.map((item, key) => (
             (<ChatItem key={key} item={item} />)
           ))
         }
-      </div>
+      </ScrollableFeed>
     </div>
   )
 }
